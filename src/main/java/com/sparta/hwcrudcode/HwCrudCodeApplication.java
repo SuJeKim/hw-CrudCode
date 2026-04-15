964abf02c6bf25f4471eabe56b598e3bcdb7cddd
jimin Kim <gjimin26@gmail.com>
2026-04-15 11:55:02 +0900

    Initial commit

---
+++ src/main/java/com/sparta/hwcrudcode/HwCrudCodeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hwcrudcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwCrudCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwCrudCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/hwcrudcode/HwCrudCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.hwcrudcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwCrudCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
